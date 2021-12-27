ed8cf9f47809833933a418edbe3b1ad259def3b5
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-12-27 00:12:47 -0500

    event listener added for C1

---
+++ index.js
@@ -0,0 +1,13 @@
+const C1 = document.getElementById("Q");
+
+C1.addEventListener("keydown", function(e){
+    if (e === 81 ) {
+        C1.click();
+    } else if (e === 113) {
+        C1.click();
+    }
+});
+
+function playSound(WAVfile) {
+    
+}
