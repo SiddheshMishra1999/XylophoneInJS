f9937380de561b2a495085bbd3a33b58b6a41881
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-12-27 01:24:15 -0500

    changed variable names to the key being pressed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const C1 = document.getElementById("Q");
-
-
+const Q = document.getElementById("Q"); //C1
+const J = document.getElementById("J"); //C5
+const Y = document.getElementById("Y"); //A1
+const M = document.getElementById("M"); //A5
+const H = document.getElementById("H"); //B1
+const W = document.getElementById("W"); //D1
+const K = document.getElementById("K"); //D5
+const E = document.getElementById("E"); //E1
+const L = document.getElementById("L"); //E5
+const R = document.getElementById("R"); //F1
+const B = document.getElementById("B"); //F5
 
 
 function playSound(WAVfile) {
@@ -8,7 +16,7 @@ function playSound(WAVfile) {
     audio.play();
 }
 
-C1.addEventListener("click",function(){
+Q.addEventListener("click",function(){
     console.log("C1 clicked");
     playSound("sounds/C1.wav");
 
