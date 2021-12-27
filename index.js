a84dfa4565169a5ce541d89ecf347c38e40bd1a8
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-12-27 00:59:16 -0500

    switch case and eventlistener added for keystrokes

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const C1 = document.getElementById("Q");
 
-C1.addEventListener("keydown", function(e){
-    if (e === 81 ) {
-        C1.click();
-    } else if (e === 113) {
-        C1.click();
-    }
-});
+
+
 
 function playSound(WAVfile) {
     
+}
+
+C1.addEventListener("click",function(){
+    console.log("C1 clicked");
+    //play sound for C1
+})
+
+
+document.onkeydown = function(e){
+    switch(e.key){
+    case "q": 
+    console.log("q");
+    //play function with C1
+    break;
+    case "Q": 
+    console.log("Q");
+    //play function with C1
+    break;
+}
+
 }
