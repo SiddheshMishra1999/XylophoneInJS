a5bbc35b0db7f27ea88e71fa9f60baa44fa815b9
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-12-27 10:43:06 -0500

    code refactoring

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const Q = document.getElementById("Q"); //C1
-const J = document.getElementById("J"); //C5
-const Y = document.getElementById("Y"); //A1
-const M = document.getElementById("M"); //A5
-const H = document.getElementById("H"); //B1
-const W = document.getElementById("W"); //D1
-const K = document.getElementById("K"); //D5
-const E = document.getElementById("E"); //E1
-const L = document.getElementById("L"); //E5
-const R = document.getElementById("R"); //F1
-const B = document.getElementById("B"); //F5
-const T = document.getElementById("T"); //G1
-const N = document.getElementById("N"); //G5
+// Keyboard keys to musical notes
+//  Q => C1
+//  J => C5
+//  Y => A1
+//  M => A5
+//  H => B1
+//  W => D1
+//  K => D5
+//  E => E1
+//  L => E5
+//  R => F1
+//  B => F5
+//  T => G1
+//  N => G5
 
 
 function playSound(WAVfile) {
@@ -18,162 +19,111 @@ function playSound(WAVfile) {
     audio.play();
 }
 
-//Adds an event listener for a button
-function keyClickEvent(key,WAVfile) {
-    key.addEventListener("click",function(){
+//Event listener to connect the click of a button to its WAV file
+function keyClickEvent(key, WAVfile) {
+    key.addEventListener("click", function () {
         playSound(WAVfile);
     })
 }
 
-keyClickEvent(Q,"sounds/C1.wav");
-keyClickEvent(W,"sounds/D1.wav");
-keyClickEvent(E,"sounds/E1.wav");
-keyClickEvent(R,"sounds/F1.wav");
-keyClickEvent(T,"sounds/G1.wav");
-keyClickEvent(Y,"sounds/A1.wav");
-keyClickEvent(H,"sounds/B1.wav");
-keyClickEvent(J,"sounds/C5.wav");
-keyClickEvent(K,"sounds/D5.wav");
-keyClickEvent(L,"sounds/E5.wav");
-keyClickEvent(B,"sounds/F5.wav");
-keyClickEvent(N,"sounds/G5.wav");
-keyClickEvent(M,"sounds/A5.wav");
+
+//Every key has same name as HTML ID, so there is no need for 
+//document.getElementById()
+keyClickEvent(Q, "sounds/C1.wav");
+keyClickEvent(W, "sounds/D1.wav");
+keyClickEvent(E, "sounds/E1.wav");
+keyClickEvent(R, "sounds/F1.wav");
+keyClickEvent(T, "sounds/G1.wav");
+keyClickEvent(Y, "sounds/A1.wav");
+keyClickEvent(H, "sounds/B1.wav");
+keyClickEvent(J, "sounds/C5.wav");
+keyClickEvent(K, "sounds/D5.wav");
+keyClickEvent(L, "sounds/E5.wav");
+keyClickEvent(B, "sounds/F5.wav");
+keyClickEvent(N, "sounds/G5.wav");
+keyClickEvent(M, "sounds/A5.wav");
 
 
 
 //Switch statement for each keypress
-document.onkeydown = function(e){
-    switch(e.key){
+document.onkeydown = function (e) {
+    switch (e.key) {
+        // When Q or q is pressed
+        case "q":
+        case "Q":
+            playSound("sounds/C1.wav");
+            break;
 
-    // When Q or q is pressed
-    case "q": 
-    console.log("q");
-    playSound("sounds/C1.wav");
-    break;
-    case "Q": 
-    console.log("Q");
-    playSound("sounds/C1.wav");
-    break;
+        // When W or w is pressed
+        case "w":
+        case "W":
+            playSound("sounds/D1.wav");
+            break;
 
+        case "e":
+        case "E":
+            playSound("sounds/E1.wav");
+            break;
 
-    // When W or w is pressed
-    case "w": 
-    console.log("w");
-    playSound("sounds/D1.wav");
-    break;
-    case "W": 
-    console.log("W");
-    playSound("sounds/D1.wav");
-    break;
+        // When R or r is pressed
+        case "r":
+        case "R":
+            playSound("sounds/F1.wav");
+            break;
 
-    case "e": 
-    console.log("e");
-    playSound("sounds/E1.wav");
-    break;
-    case "E": 
-    console.log("E");
-    playSound("sounds/E1.wav");
-    break;
+        // When T or t is pressed
+        case "t":
+        case "T":
+            playSound("sounds/G1.wav");
+            break;
 
-    // When R or r is pressed
-    case "r": 
-    console.log("r");
-    playSound("sounds/F1.wav");
-    break;
-    case "E": 
-    console.log("R");
-    playSound("sounds/F1.wav");
-    break;
+        // When Y or y is pressed
+        case "y":
+        case "Y":
+            playSound("sounds/A1.wav");
+            break;
 
-    // When T or t is pressed
-    case "t": 
-    console.log("t");
-    playSound("sounds/G1.wav");
-    break;
-    case "E": 
-    console.log("T");
-    playSound("sounds/G1.wav");
-    break;
-    
-    // When Y or y is pressed
-    case "y": 
-    console.log("y");
-    playSound("sounds/A1.wav");
-    break;
-    case "Y": 
-    console.log("Y");
-    playSound("sounds/A1.wav");
-    break;
+        // When H or h is pressed
+        case "h":
+        case "H":
+            playSound("sounds/B1.wav");
+            break;
 
-    // When H or h is pressed
-    case "h": 
-    console.log("h");
-    playSound("sounds/B1.wav");
-    break;
-    case "H": 
-    console.log("H");
-    playSound("sounds/B1.wav");
-    break;
+        // When J or j is pressed
+        case "j":
+        case "J":
+            playSound("sounds/C5.wav");
+            break;
 
-    // When J or j is pressed
-    case "j": 
-    console.log("j");
-    playSound("sounds/C5.wav");
-    break;
-    case "J": 
-    console.log("J");
-    playSound("sounds/C5.wav");
-    break;
+        // When K or k is pressed
+        case "k":
+        case "K":
+            playSound("sounds/D5.wav");
+            break;
 
-    // When K or k is pressed
-    case "k": 
-    console.log("k");
-    playSound("sounds/D5.wav");
-    break;
-    case "K": 
-    console.log("K");
-    playSound("sounds/D5.wav");
-    break;
+        // When L or l is pressed
+        case "l":
+        case "L":
+            playSound("sounds/E5.wav");
+            break;
 
-    // When L or l is pressed
-    case "l": 
-    console.log("l");
-    playSound("sounds/E5.wav");
-    break;
-    case "L": 
-    console.log("L");
-    playSound("sounds/E5.wav");
-    break;
+        // When B or b is pressed
+        case "b":
+        case "B":
+            playSound("sounds/F5.wav");
+            break;
 
-    // When B or b is pressed
-    case "b": 
-    console.log("b");
-    playSound("sounds/F5.wav");
-    break;
-    case "B": 
-    console.log("B");
-    playSound("sounds/F5.wav");
-    break;
+        // When N or n is pressed
+        case "n":
+        case "N":
+            playSound("sounds/G5.wav");
+            break;
 
-    // When N or n is pressed
-    case "n": 
-    console.log("n");
-    playSound("sounds/G5.wav");
-    break;
-    case "N": 
-    console.log("N");
-    playSound("sounds/G5.wav");
-    break;
-
-    // When M or m is pressed
-    case "m": 
-    console.log("m");
-    playSound("sounds/A5.wav");
-    break;
-    case "M": 
-    console.log("M");
-    playSound("sounds/A5.wav");
-    break;
-}
+        // When M or m is pressed
+        case "m":
+        case "M":
+            playSound("sounds/A5.wav");
+            break;
+    }
 
 }
